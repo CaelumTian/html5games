57f866abf9a879481a25bc829c78852076764c50
T-phantom <758962035@qq.com>
2015-11-17 15:29:15 +0800

    添加基类状态，测试移动

--- KingdomRush/src/Enemy/MonsterBase.js
+++ KingdomRush/src/Enemy/MonsterBase.js
@@ -7,13 +7,68 @@ var MonsterBase = cc.Sprite.extend({
     _hp : null,                    //当前血量
     _life : null,                  //最大生命值
     _lifeBar : null,               //血条
-    ctor : function() {
-        this._super();
-        this._fsm = new StateMachine(this);
-    },
-    init : function(plistRes, pngRes) {
-        /*给这个精灵赋初值*/
+    _prefix : "",
+    _walkF : null,
+    _walkB : null,
+    _walkX : null,
+    _rightFlag : true,             //默认向右方式
+    ctor : function(plistRes, pngRes, defaultImg, prefix) {
         cc.spriteFrameCache.addSpriteFrames(plistRes);
         cc.SpriteBatchNode.create(pngRes);
+        this._super(defaultImg);
+        this.init(plistRes, pngRes, prefix);
+    },
+    init : function(plistRes, pngRes, prefix) {
+        /*给这个精灵赋初值 prefix前缀 "m1-" */
+        this._prefix = prefix;
+        this._walkB = new cc.Animation();
+        this._walkF = new cc.Animation();
+        this._walkX = new cc.Animation();
+        for(var i = 0; i < 12; i++) {
+            var walkF = prefix + "-f-walk" + (i + 1) + ".png",
+                walkB = prefix + "-b-walk" + (i + 1) + ".png",
+                walkX = prefix + "-x-walk" + (i + 1) + ".png";
+            this._walkF.addSpriteFrame(cc.spriteFrameCache.getSpriteFrame(walkF));
+            this._walkB.addSpriteFrame(cc.spriteFrameCache.getSpriteFrame(walkB));
+            this._walkX.addSpriteFrame(cc.spriteFrameCache.getSpriteFrame(walkX));
+        }
     }
 });
+MonsterBase.prototype.changeWalkState = function(state) {
+    var animate = null;
+    switch(state) {
+        case "go_right" :
+            this.stopAllActions();
+            if(!this._rightFlag) {
+                this.scaleX = -1;
+            }
+            this._walkX.setDelayPerUnit(1 / 12);
+            animate = cc.animate(this._walkX).repeatForever();
+            break;
+        case "go_left" :
+            this.stopAllActions();
+            if(this._rightFlag) {
+                this.scaleX = -1;
+            }
+            this._walkX.setDelayPerUnit(1 / 12);
+            animate = cc.animate(this._walkX).repeatForever();
+            break;
+        case "go_back" :
+            this.stopAllActions();
+            this._walkB.setDelayPerUnit(1 / 12);    //顺序不能颠倒
+            animate = cc.animate(this._walkB).repeatForever();
+            break;
+        case "go_front" :
+            this.stopAllActions();
+            this._walkF.setDelayPerUnit(1 / 12);
+            animate = cc.animate(this._walkF).repeatForever();
+            break;
+        case "stop" :
+            this.stopAllActions();
+            return false;
+        default :
+            throw new Error("状态切换错误");
+            break;
+    }
+    this.runAction(animate);
+};
--- KingdomRush/src/GameScene.js
+++ KingdomRush/src/GameScene.js
@@ -1,6 +1,8 @@
 /**
  * Created by CaelumTian on 2015/11/11.
  */
+
+var monster = null;
 var GameScene = cc.Scene.extend({
     _gameData : null,
     ctor : function(num) {
@@ -15,8 +17,9 @@ var GameScene = cc.Scene.extend({
             if(err) {
                 throw new Error("关卡数据加载错误");
             }
-            console.log(result);
             self._gameData = result;
+            currRes = result["res"];
+            console.log(currRes);
             layer.initWithResources(currRes, self._init, self);
         });
     },
@@ -25,6 +28,27 @@ var GameScene = cc.Scene.extend({
             gameData = target._gameData;
         console.log("执行回调函数了");
         console.log("关卡数据");
+        self._setMap();
+    },
+    _setMap : function() {
+        var centerX = cc.winSize.width / 2,
+            centerY = cc.winSize.height / 2;
+
+        var map = new cc.Sprite("res/Map/map1.jpg");
+        map.attr({
+            "x" : centerX,
+            "y" : centerY
+        });
+        this.addChild(map);              //最优先加载地图
+
+        var monster1 = new MonsterBase("res/Enemy/monster1.plist", "res/Enemy/monster1.png", "#m1-x-walk1.png", "m1");
+        monster = monster1;
+        monster1.attr({
+            "x" : centerX,
+            "y" : centerY
+        });
+        this.addChild(monster1);
+        monster1.changeWalkState("go_back");
     },
     onExit : function() {
         console.log("这里被执行了");
